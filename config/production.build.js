3ef175d56a0cd728227248f588e2f86ac0ead0e1
Ankur Agarwal <signup.ankur@gmail.com>
2012-10-13 02:05:29 +0530

    Enabling minification in build script

--- config/production.build.js
+++ config/production.build.js
@@ -67,7 +67,7 @@
     //- "closure.keepLines": Same as closure option, but keeps line returns
     //in the minified files.
     //- "none": no minification will be done.
-    optimize: "none",
+    optimize: "uglify",
 
     //If using UglifyJS for script optimization, these config options can be
     //used to pass configuration values to UglifyJS.
